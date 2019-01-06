b2447a2f070c4a14c01cb816c5eb449f7c51fd55
Darío Reyes <eran6reyes@gmail.com>
2019-01-06 03:23:26 +0000

    Adds entity

---
+++ lib/nulu/physics/entity.rb
@@ -0,0 +1,45 @@
+module Nulu
+
+  class Entity
+
+    attr_accessor :velocity
+    attr_accessor :mass, :friction
+
+    def initialize(shape, mass, friction = 0.0, initial_velocity = Nulu::Vector.new(0, 0))
+      @shape = shape
+      @mass = Float(mass)
+      @friction = Float(friction)
+      @velocity = initial_velocity
+    end
+
+
+    ## Shape Delegation
+    def width() @shape.width() end  
+    def height() @shape.height() end
+    def center() @shape.center() end
+    def left() @shape.left() end
+    def right() @shape.right() end
+    def top() @shape.top() end
+    def bottom() @shape.bottom() end
+    def center=(new_center) @shape.center = new_center end
+    def left=(new_left) @shape.left = new_left end
+    def right=(new_right) @shape.right = new_right end
+    def top=(new_top) @shape.top = new_top end
+    def bottom=(new_bottom) @shape.bottom = new_bottom end
+    def move(offset) @shape.move(offset) end
+    def move_x(offset_x) @shape.move_x(offset_x) end
+    def move_y(offset_y) @shape.move_y(offset_y) end
+
+    def shape # exposed for debug drawing
+      @shape
+    end
+
+
+    ## Loop
+    # TODO: Change name?
+    def update(delta)
+      @shape.move(@velocity * delta)
+    end
+    
+  end
+end
