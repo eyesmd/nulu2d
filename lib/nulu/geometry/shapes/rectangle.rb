5d6ffb28b52f9971fb77be6c56e5e043a4833f45
Darío Reyes <eran6reyes@gmail.com>
2019-01-10 18:49:43 +0000

    Changes Rectangle contstructor

--- lib/nulu/geometry/shapes/rectangle.rb
+++ lib/nulu/geometry/shapes/rectangle.rb
@@ -3,13 +3,13 @@ module Nulu
   class Rectangle < Nulu::Polygon
 
   	def initialize(width, height, initial_pos = Nulu::Point.new(0, 0))
-  		initial_x = initial_pos.x
-  		initial_y = initial_pos.y
-  		super(Nulu::Point.new(initial_x - width/2.0, initial_y - height/2.0),
-  			  Nulu::Point.new(initial_x + width/2.0, initial_y - height/2.0),
-  			  Nulu::Point.new(initial_x + width/2.0, initial_y + height/2.0),
-  			  Nulu::Point.new(initial_x - width/2.0, initial_y + height/2.0))
-  	end
+      initial_x = initial_pos.x
+      initial_y = initial_pos.y
+      super(Nulu::Point.new(initial_x, initial_y),
+          Nulu::Point.new(initial_x + width, initial_y),
+          Nulu::Point.new(initial_x + width, initial_y + height),
+          Nulu::Point.new(initial_x, initial_y + height))
+    end
 
   end
   
